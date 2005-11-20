19711991aee9bf7d5d1f75ff0d8af72f664709e4
Michael Richardson <mcr@marajade.sandelman.ca>
2005-11-19 21:50:44 -0500

    move configuration files to /etc/l2tpd

--- file.h
+++ file.h
@@ -49,10 +49,10 @@ struct host
 #define SENSE_ALLOW -1
 #define SENSE_DENY 0
 
-#define DEFAULT_AUTH_FILE "/etc/l2tp/l2tp-secrets"
-#define ALT_DEFAULT_AUTH_FILE "/etc/l2tpd/l2tp-secrets"
-#define DEFAULT_CONFIG_FILE "/etc/l2tp/l2tpd.conf"
-#define ALT_DEFAULT_CONFIG_FILE "/etc/l2tpd/l2tpd.conf"
+#define DEFAULT_AUTH_FILE "/etc/l2tpd/l2tp-secrets"
+#define ALT_DEFAULT_AUTH_FILE "/etc/l2tp/l2tp-secrets"
+#define DEFAULT_CONFIG_FILE "/etc/l2tpd/l2tpd.conf"
+#define ALT_DEFAULT_CONFIG_FILE "/etc/l2tp/l2tpd.conf"
 #define DEFAULT_PID_FILE "/var/run/l2tpd.pid"
 
 /* Definition of an LNS */
