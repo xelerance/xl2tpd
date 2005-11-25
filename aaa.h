f09c91543dc84206a7a82db62bab09b8a52da44a
Michael Richardson <mcr@xelerance.com>
2005-11-25 13:41:36 -0500

    remove definition of uaddr[] --- it is not needed at present (cherry picked from 148ec348a69f28608d2bc671e96222a4c595d83d commit)

--- aaa.h
+++ aaa.h
@@ -47,7 +47,6 @@ struct challenge
 };
 
 extern struct lns *get_lns (struct tunnel *);
-extern struct addr_ent *uaddr[];
 extern unsigned int get_addr (struct iprange *);
 extern void reserve_addr (unsigned int);
 extern void unreserve_addr (unsigned int);
