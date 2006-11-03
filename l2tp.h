66ee9daf76fcd04dae9976be8a43124baf04afa7
Paul Wouters <paul@xelerance.com>
2006-11-03 10:50:32 -0500

    Updated version.

--- l2tp.h
+++ l2tp.h
@@ -37,7 +37,7 @@ typedef unsigned long long _u64;
 #define CONTROL_PIPE "/var/run/l2tp-control"
 
 #define BINARY "l2tpd"
-#define SERVER_VERSION "1.04-X"
+#define SERVER_VERSION "xl2tpd-1.1.05"
 #define VENDOR_NAME "xelerance.com"
 #ifndef PPPD
 #define PPPD		"/usr/sbin/pppd"
