xelerance/xl2tpd

commit 90368df06394a26639341a50fac17a87e188c638

Fix control buffer handling in udp_xmit
Assign control buffer only if we put something there, this makes code
compatible with Linux and OpenBSD again.

This change uses approach from 62b6b1960d4de985bbf56e723a4b8060f888ed84
(Fix udp_xmit to work on Linux *and* OpenBSD)

Fix: 4830a2008917f4882a2b293e3b22dce2f9af570e (use address from the last
received UDP packet in UDP messages response)

network.c
@@ -274,23 +274,18 @@ void control_xmit (void *b)
 
 void udp_xmit (struct buffer *buf, struct tunnel *t)
 {
-    struct cmsghdr *cmsg;
+    struct cmsghdr *cmsg = NULL;
     char cbuf[CMSG_SPACE(sizeof (unsigned int) + sizeof (struct in_pktinfo))];
     unsigned int *refp;
     struct msghdr msgh;
     int err;
     struct iovec iov;
-    int finallen;
+    int finallen = 0;
 
     /*
      * OKAY, now send a packet with the right SAref values.
      */
     memset(&msgh, 0, sizeof(struct msghdr));
-
-    cmsg = NULL;
-    msgh.msg_control = cbuf;
-    msgh.msg_controllen = sizeof(cbuf);
-    finallen = 0;
 
     if (gconfig.ipsecsaref && t->refhim != IPSEC_SAREF_NULL) {
 	cmsg = CMSG_FIRSTHDR(&msgh);
@@ -329,7 +324,16 @@ void udp_xmit (struct buffer *buf, struct tunnel *t)
     }
 #endif
 
-    msgh.msg_controllen = finallen;
+    /*
+     * Some OS don't like assigned buffer with zero length (e.g. OpenBSD),
+     * some OS don't like empty buffer with non-zero length (e.g. Linux).
+     * So make them all happy by assigning control buffer only if we really
+     * have something there.
+     */
+    if (finallen) {
+        msgh.msg_control = cbuf;
+        msgh.msg_controllen = finallen;
+    }
 
     iov.iov_base = buf->start;
     iov.iov_len  = buf->len;

commit 551b92fd691d8a6ce2e9d32f15dfbe252b2c434d

Fixing minor bug in Linux that was introduced by 90368df06394a26639341a50fac17a87e188c638
msg structure needs to be defined earlier so cmsg can be set properly for Linux

network.c
@@ -303,6 +303,9 @@ void udp_xmit (struct buffer *buf, struct tunnel *t)
     }
 
 #ifdef LINUX
+    msgh.msg_control = cbuf;
+    msgh.msg_controllen = sizeof(cbuf);
+
     if (t->my_addr.ipi_addr.s_addr){
 	struct in_pktinfo *pktinfo;
 
@@ -322,6 +325,8 @@ void udp_xmit (struct buffer *buf, struct tunnel *t)
 
 	finallen += cmsg->cmsg_len;
     }
+
+    msgh.msg_controllen = finallen;
 #endif
 
     /*