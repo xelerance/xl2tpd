a3192126714adf8523b30e783ebf9cc8e453d06d
Pau Espin Pedrol <pespin.shar@gmail.com>
2019-03-03 01:02:57 +0100

    xl2tpd: start_pppd: Fix truncation of last character
    Maximum integer value in 32 bits is 2147483647, which is 10 char long.
    An extra char is required for null terminated strings.
    
    xl2tpd.c:454:38: warning: ‘snprintf’ output may be truncated before the last format character [-Wformat-truncation=]
            snprintf (stropt[pos], 10, "%d", fd2);
                                          ^
    xl2tpd.c:454:8: note: ‘snprintf’ output between 2 and 11 bytes into a destination of size 10
            snprintf (stropt[pos], 10, "%d", fd2);

--- xl2tpd.c
+++ xl2tpd.c
@@ -450,18 +450,18 @@ int start_pppd (struct call *c, struct ppp_opts *opts)
        stropt[pos++] = strdup ("plugin");
        stropt[pos++] = strdup ("pppol2tp.so");
        stropt[pos++] = strdup ("pppol2tp");
-       stropt[pos] = malloc (10);
-       snprintf (stropt[pos], 10, "%d", fd2);
+       stropt[pos] = malloc (11);
+       snprintf (stropt[pos], 11, "%d", fd2);
         pos++;
        if (c->container->lns) {
         stropt[pos++] = strdup ("pppol2tp_lns_mode");
         stropt[pos++] = strdup ("pppol2tp_tunnel_id");
-        stropt[pos] = malloc (10);
-        snprintf (stropt[pos], 10, "%d", c->container->ourtid);
+        stropt[pos] = malloc (11);
+        snprintf (stropt[pos], 11, "%d", c->container->ourtid);
             pos++;
         stropt[pos++] = strdup ("pppol2tp_session_id");
-        stropt[pos] = malloc (10);
-        snprintf (stropt[pos], 10, "%d", c->ourcid);
+        stropt[pos] = malloc (11);
+        snprintf (stropt[pos], 11, "%d", c->ourcid);
             pos++;
        }
     }
