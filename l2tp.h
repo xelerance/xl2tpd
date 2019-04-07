b9378207e67eb84047cc682e5ac1669916cb1e8f
Samir Hussain <build@openswan.org>
2019-04-07 12:31:20 -0400

    Update VERSION to 1.3.14rc1

--- l2tp.h
+++ l2tp.h
@@ -59,7 +59,7 @@ typedef unsigned long long _u64;
 #define CONTROL_PIPE_REQ_LNS_REMOVE 'w'     /* Get status of LNS */
 
 #define BINARY "xl2tpd"
-#define SERVER_VERSION "xl2tpd-1.3.14dev1"
+#define SERVER_VERSION "xl2tpd-1.3.14rc1"
 #define VENDOR_NAME "xelerance.com"
 #ifndef PPPD
 #define PPPD		"/usr/sbin/pppd"
